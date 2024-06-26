f3109ea6eeea0e55a2600b2ef271010bdcdc04fa
hibaasim <hiba.asim10@gmail.com>
2024-06-26 14:30:14 -0700

    task-4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Prepares a MySQL server for the project (test)
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CRAETE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
