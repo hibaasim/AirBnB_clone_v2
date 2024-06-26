98123716edc3457195c6808c1a8ac4bb92d2bccd
hibaasim <hiba.asim10@gmail.com>
2024-06-27 01:10:35 +0300

    task-5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,'Review': Review}
 
 
 class FileStorage:
@@ -8,8 +18,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            if isinstance(cls, str):
+                cls = globals().get(cls)
+            if cls and issubclass(cls, BaseModel):
+                cls_dict = (k: v for k, v in self.__objects.items()
+                            if isinstance(v, cls))
+                return cls_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -27,14 +44,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -48,3 +57,17 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+        def delete(self, obj=None):
+            """Delete obj from __objects if it’s inside - if obj is equal
+            to None, the method should not do anything"""
+            if obj is None:
+                return
+            obj_delete = f"{obj.__class__.__name__}.{obj.id}"
+
+            try:
+                del FileStorage.__objects[obj_delete]
+            except AttributeError:
+                pass
+            except KeyboardInterrupt:
+                pass
