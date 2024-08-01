b61a1af8ccc25500e703d483c0e3e32f2b7d34cd
hibaasim <hiba.asim10@gmail.com>
2024-07-31 18:02:03 -0700

    few more

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return a given string"""
+    return ("HBNB")
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Return C <text>"""
+    text = text.replace("_", " ")
+    return (f"C {text}")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
